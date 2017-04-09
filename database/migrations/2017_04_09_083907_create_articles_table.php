ccf4df31cb5c8acefa3ac54a83e2d48b8de1e1ab
Sliverwing <tlv591208510@gmail.com>
2017-04-09 17:07:56 +0800

    add article model & migration

---
+++ database/migrations/2017_04_09_083907_create_articles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->boolean('is_direct_link')->default(false);
+            $table->boolean('is_top')->default(false);
+            $table->string('intro')->nullable();
+            $table->integer('views_count')->default(0);
+            $table->string('banner')->nullable();
+            $table->string('reference_link')->nullable();
+            $table->text('content')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
