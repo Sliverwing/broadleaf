15953e090f1f5d60d487d949af8d1c7b9ff8c5b7
Sliverwing <tlv591208510@gmail.com>
2017-04-11 20:25:19 +0800

    add @getRolePermissions & @getPermissionForNavBar

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laratrust\Traits\LaratrustUserTrait;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -28,4 +29,29 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getRolePermissions()
+    {
+        return DB::table('permissions')
+            ->join('permission_role', 'permission_role.permission_id', '=', 'permissions.id')
+            ->join('roles', 'roles.id', '=', 'permission_role.role_id' )
+            ->join('role_user', 'role_user.role_id', '=', 'permission_role.role_id')
+            ->join('users', 'users.id', '=', 'role_user.user_id')
+            ->where('users.id', $this->id)
+            ->select('permissions.*')
+            ->get();
+    }
+
+    public function getPermissionForNavBar()
+    {
+        return DB::table('permissions')
+            ->join('permission_role', 'permission_role.permission_id', '=', 'permissions.id')
+            ->join('roles', 'roles.id', '=', 'permission_role.role_id' )
+            ->join('role_user', 'role_user.role_id', '=', 'permission_role.role_id')
+            ->join('users', 'users.id', '=', 'role_user.user_id')
+            ->where('users.id', $this->id)
+            ->where('is_url_enabled', 1)
+            ->select('permissions.*')
+            ->get();
+    }
 }
