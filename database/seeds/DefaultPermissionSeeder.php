895406c875a929ed1843cb8e6497b8b1fe52e423
Sliverwing <tlv591208510@gmail.com>
2017-04-11 17:29:13 +0800

    update PermissionSeeder

--- database/seeds/DefaultPermissionSeeder.php
+++ database/seeds/DefaultPermissionSeeder.php
@@ -17,5 +17,39 @@ class DefaultPermissionSeeder extends Seeder
             'display_name' => '管理员登陆',
             'description' => '管理员登陆',
         ]);
+//        Permission
+        $permission_index = Permission::firstOrCreate([
+            'name' => 'permission.index',
+            'display_name' => '权限管理',
+            'is_url_enabled' => true,
+            'url' => '/admin/permission'
+        ]);
+        Permission::firstOrCreate([
+            'name' => 'permission.store',
+            'display_name' => '保存权限',
+        ]);
+        Permission::firstOrCreate([
+            'name' => 'permission.create',
+            'display_name' => '新建权限',
+            'parent_id' => $permission_index->id
+        ]);
+        Permission::firstOrCreate([
+            'name' => 'permission.destroy',
+            'display_name' => '删除权限',
+        ]);
+        Permission::firstOrCreate([
+            'name' => 'permission.update',
+            'display_name' => '更新权限',
+        ]);
+        Permission::firstOrCreate([
+            'name' => 'permission.show',
+            'display_name' => '显示权限详情',
+        ]);
+        Permission::firstOrCreate([
+            'name' => 'permission.edit',
+            'display_name' => '编辑权限',
+        ]);
+
+
     }
 }
