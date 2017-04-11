734c19764b6683ce0e67cf99854b48de36ffe6d2
Sliverwing <tlv591208510@gmail.com>
2017-04-11 20:26:30 +0800

    display permissions to nav bar

--- resources/views/admin/components/aside.blade.php
+++ resources/views/admin/components/aside.blade.php
@@ -5,19 +5,11 @@
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
-            <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
-            <li class="treeview">
-                <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-                    <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="#">Link in level 2</a></li>
-                    <li><a href="#">Link in level 2</a></li>
-                </ul>
-            </li>
+            @foreach($permissions as $permission)
+                <li>
+                    <a href="{{ $permission->url }}"><i class="fa fa-link"></i> {{ $permission->display_name }}</a>
+                </li>
+            @endforeach
         </ul>
         <!-- /.sidebar-menu -->
     </section>
