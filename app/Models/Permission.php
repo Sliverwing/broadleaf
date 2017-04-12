79fb5fc02ba3c1599523ed69f7e8afb7058e9178
Sliverwing <tlv591208510@gmail.com>
2017-04-12 09:25:20 +0800

    update

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -6,5 +6,13 @@ use Ultraware\Roles\Models\Permission as BasePermission;
 
 class Permission extends BasePermission
 {
-    //
+    protected $fillable = [
+        'slug',
+        'name',
+        'description',
+        'model',
+        'is_url_enabled',
+        'url',
+        'order',
+    ];
 }
