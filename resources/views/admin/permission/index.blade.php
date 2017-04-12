5be9f3c90e7f398ee15fe65b324fef143943dd2f
Sliverwing <tlv591208510@gmail.com>
2017-04-12 09:30:49 +0800

    include message component

--- resources/views/admin/permission/index.blade.php
+++ resources/views/admin/permission/index.blade.php
@@ -1,6 +1,7 @@
 @extends('admin.index')
 
 @section('content')
+    @include('components.message')
     <div class="box">
         <div class="box-header with-border">
             <h3 class="box-title">权限表</h3>
