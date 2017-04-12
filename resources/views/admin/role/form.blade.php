41cfbb3cf88bbaadf6c9aeb3531ff538a889b6b0
Sliverwing <tlv591208510@gmail.com>
2017-04-12 20:15:25 +0800

    add /admin/role view resources

---
+++ resources/views/admin/role/form.blade.php
@@ -0,0 +1,66 @@
+@extends('admin.index')
+
+@section('content')
+    @include('components.message')
+    <div class="box box-primary">
+        <div class="box-header with-border">
+            <h3 class="box-title">编辑</h3>
+        </div>
+        <!-- /.box-header -->
+        <!-- form start -->
+        @php
+        $base = '/admin/role/';
+        $actionUrl = isset($item) ? $base . $item->id : $base;
+        $method = isset($item) ? 'PUT' : 'POST';
+        @endphp
+        <form action="{{ $actionUrl }}" method="POST">
+            <input type="hidden" name="_method" value="{{ $method }}">
+            {{ csrf_field() }}
+            <div class="box-body">
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="name">名称</label>
+                    <input type="text" name="name" value="{{ (old('name') != null ? old('name') : (isset($item) ? $item->name : '')) }}" class="form-control" id="name">
+                    @if ($errors->has('name'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('name') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group{{ $errors->has('slug') ? ' has-error' : '' }}">
+                    <label for="slug">标识符</label>
+                    <input type="text" name="slug" value="{{ (old('slug') != null ? old('slug') : (isset($item) ? $item->slug : '')) }}" class="form-control" id="slug">
+                    <p class="help-block">请勿随意更改!</p>
+                    @if ($errors->has('slug'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('slug') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
+                    <label for="description">描述</label>
+                    <input type="text" name="description" value="{{ (old('description') != null ? old('description') : (isset($item) ? $item->description : '')) }}" class="form-control" id="description">
+                    @if ($errors->has('description'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('description') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group{{ $errors->has('level') ? ' has-error' : '' }}">
+                    <label for="level">角色等级</label>
+                    <input type="text" name="level" value="{{ (old('level') != null ? old('level') : (isset($item) ? $item->level : '')) }}" class="form-control" id="level">
+                    <p class="help-block">请勿随意更改! 0 为最小值</p>
+                    @if ($errors->has('level'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('level') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+            <!-- /.box-body -->
+
+            <div class="box-footer">
+                <button type="submit" class="btn btn-primary">保存</button>
+            </div>
+        </form>
+    </div>
+@endsection
---
+++ resources/views/admin/role/index.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.index')
+
+@section('content')
+    @include('components.message')
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">用户角色表</h3>
+        </div>
+        <div class="box-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>显示名称</th>
+                    <th>标识</th>
+                    <th>描述</th>
+                    <th>等级</th>
+                    <th>更新于</th>
+                    <th>操作</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($items as $item)
+                    <tr>
+                        <th>{{ $item->id }}</th>
+                        <th>{{ $item->name }}</th>
+                        <th>{{ $item->slug }}</th>
+                        <th>{{ $item->description }}</th>
+                        <th>{{ $item->level }}</th>
+                        <th>{{ $item->updated_at }}</th>
+                        <th> @permission('role.edit') <a href="/admin/role/{{ $item->id }}/edit" class="btn btn-default">编辑</a>@endpermission
+                        @permission('role.destroy') <a href="#" onclick="handleDeleteBtn('/admin/role/{{ $item->id }}')" id="delete-btn" class="btn btn-danger">删除</a>@endpermission</th>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+        <div class="box-footer clearfix">
+            <ul class="pagination pagination-sm no-margin">
+                @permission('role.create')
+                <a href="/admin/role/create" class="btn btn-default new-btn">新建</a>
+                @endpermission
+                <div class="pull-right">
+                    {{ $items->links() }}
+                </div>
+            </ul>
+        </div>
+    </div>
+    @include('components.delete')
+@endsection
