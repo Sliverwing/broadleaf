d2d3ed5c642d988faacacefcc5a8a72a027b75cb
Sliverwing <tlv591208510@gmail.com>
2017-04-19 22:32:58 +0800

    update  attr

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'is_direct_link',
+        'is_top',
+        'intro',
+        'views_count',
+        'banner',
+        'reference_link',
+        'content',
+        'user_id',
+    ];
 }
