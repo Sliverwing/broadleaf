eab844a802d3f67a14c00f025ba8e9e0bea47281
Sliverwing <tlv591208510@gmail.com>
2017-04-23 18:06:38 +0800

    update relationship

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -18,4 +18,14 @@ class Article extends Model
         'user_id',
         'category_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(\App\User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(\App\Models\Category::class);
+    }
 }
