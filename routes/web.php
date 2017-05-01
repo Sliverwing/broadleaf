154c9d42cc9bd71c746aed36065f8dc5dcf5fe7b
Sliverwing <tlv591208510@gmail.com>
2017-05-01 21:25:04 +0800

    add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function() {
     Route::get('/', ['uses' => 'IndexController@index', 'as' => 'admin.login']);
     Route::resource('/permission', 'PermissionController');
     Route::resource('/role', 'RoleController');
